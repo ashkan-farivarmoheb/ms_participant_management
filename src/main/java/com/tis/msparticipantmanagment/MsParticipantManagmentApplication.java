9a732c1573a76d252810bd984749332015025443
ashkan-farivarmoheb <ashkan.farivarmoheb@gmail.com>
2023-06-01 23:39:30 +1000

    first commit

---
+++ src/main/java/com/tis/msparticipantmanagment/MsParticipantManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.tis.msparticipantmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsParticipantManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsParticipantManagmentApplication.class, args);
+	}
+
+}
